8b30cfe3db4675f1f2eb71f7194b64375944a129
Dmitriy <Dmitriy@DESKTOP-8IVVVVB>
2022-10-16 13:21:55 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] strArray = str.Split();
+            string min = strArray[0];
+            foreach (string s in strArray)
+            {
+                if (s.Length > min.Length)
+                {
+                    min = s;
+                }
+            }
+            Console.WriteLine(min);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str2 = "";
+            string str3 = "";
+            foreach (char c in str)
+            {
+                str2 = c + str2.Trim().ToLower();
+                str3 = str3.Trim().ToLower() + c;
+            }
+            if (str2 == str3)
+            {
+                Console.WriteLine("Фраза является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Фраза не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
